1ababcca11634f8f91f78e839b7cadd88c58bc6d
Vanessa Patterson <vpatt1031@gmail.com>
2021-04-06 14:34:56 -0400

    attempting to deploy on heroku and mongo atlas

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true 
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/budget',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 // routes
 app.use(require("./routes/api.js"));
